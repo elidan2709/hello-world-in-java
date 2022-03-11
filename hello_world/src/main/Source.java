f5e07234e85052d371098f6945070ddd3fc9fe08
Элишаков Даниил <dan-eli@mail.ru>
2022-03-11 17:21:57 +0300

    bepis

---
+++ hello_world/src/main/Source.java
@@ -0,0 +1,16 @@
+package main;
+/*
+ * Source - ehfejhf
+ */
+public class Source {
+
+	public Source() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
